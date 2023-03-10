c207a3d3d76e94cadca1af4bb1a13570fea0342c
Benjamin Fonbuena <bchfonz@gmail.com>
2023-03-09 21:31:20 -0700

    Added login page and started javascript for game

---
+++ game.js
@@ -0,0 +1,9 @@
+function makeEvent(){
+    const dateEl = document.querySelector('#gameDate');
+    const locationEl = document.querySelector('#location');
+    window.location.href = "sched.html";
+}
+
+/*Make a vector to store the events and store it in the local storage.
+Include in the vecors the date, location, and a list of people that are 
+attending the event. */
