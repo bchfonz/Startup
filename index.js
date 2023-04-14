bd58f046aa6852b3e884e092c087f8b209b1b00c
Carson <koehlecarson@gmail.com>
2023-04-13 21:25:16 -0600

    added quote stuff, not done yet tho

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
-
+const port = process.argv.length > 2 ? process.argv[2] : 4000;  //A ta said that startup should be hosted on localport:4000
+                                                                //and simon was localport:3000, so I switched the vals
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
