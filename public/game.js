9e25a89605e13f74b5f1b0ed4b85fa8565e02386
Carson <koehlecarson@gmail.com>
2023-04-17 17:29:00 -0600

    fixed naming convention bug, location should work

--- public/game.js
+++ public/game.js
@@ -1,24 +1,29 @@
 function makeEvent(){
-    let dateEl = document.querySelector('#gameDate').value;
     console.log("Make event called")
+    let dateEl = document.querySelector('#gameDate').value;
     let timeEl = document.querySelector('#gameTime').value;
-    let locationEl = document.querySelector('#location').value;
-    let attendees = 1;
+    let locationEl = document.querySelector('#gameLocation').value;
+    console.log("Check location");
     
+    console.log("Check 1");
     let eventsString;
     let eventsData;
     if (localStorage.getItem('events') != null) {
         eventsString = localStorage.getItem('events');
         eventsData = JSON.parse(eventsString);
+        console.log("non-null eventsString")
     }
     else {
         eventsString = [];
         eventsData = [];
+        console.log("null eventsString");
     }
     console.log(eventsData);
     let newEvent = {location : locationEl, eventDate : dateEl, time : timeEl, people : attendees}
+    console.log("Check 2");
     eventsData.push(newEvent);
     localStorage.setItem('events', JSON.stringify(eventsData));
+    console.log("Check 3");
     window.location.href = "sched.html";
 }
 
